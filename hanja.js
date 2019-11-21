bc83389c7e5970b0601ae7b397b62b6feff19b8a
Simhyeon <aracus755@gmail.com>
2019-11-22 01:35:37 +0900

    Initial Commit

---
+++ hanja.js
@@ -0,0 +1,29 @@
+var hanjaList;
+var hangulList;
+
+function reset() {
+	for (var i=0; i< hangulList.length; i++) {
+		hangulList[i].style.display = "none";
+	}
+}
+
+function init() {
+	hanjaList = document.getElementsByClassName("hanja");
+	hangulList = document.getElementsByClassName("hangul");
+	for (var i=0; i< hanjaList.length; i++) {
+		hanjaList[i].setAttribute("id", i);
+	}
+}
+
+function debug() {
+	for(var i=0;i< hangulList.length; i++) {
+		hangulList[i].style.display = "unset";
+	}
+}
+
+function toggleText(self) {
+	console.log(hanjaList.length);
+	console.log(hangulList.length);
+	console.log(self.id);
+	hangulList[self.id].style.display = "unset";
+}
